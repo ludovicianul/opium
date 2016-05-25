36c069ba0c526e4f92712a510daf9d2c83d00b02
ludovicianul <iliemad@gmail.com>
2016-05-25 21:07:55 +0300

    Enable Swagger

--- src/main/java/com/insidecoding/opium/Application.java
+++ src/main/java/com/insidecoding/opium/Application.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @ComponentScan
 @EnableAutoConfiguration
 @EnableScheduling
+@EnableSwagger2
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
